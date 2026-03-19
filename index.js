e648f186f6f12b25043bd7fada46f5f3f66fe9e3
karolmejiavera88k-glitch <karol.mejia.vera88k@gmail.com>
2026-03-18 20:45:28 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const VERIFY_TOKEN = "12345";
+
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode && token === VERIFY_TOKEN) {
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
+});
+
+app.post("/webhook", (req, res) => {
+  console.log("Mensaje recibido:", JSON.stringify(req.body, null, 2));
+  res.sendStatus(200);
+});
+
+app.listen(3000, () => console.log("Servidor listo"));
